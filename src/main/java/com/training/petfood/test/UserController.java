10cb2f3e136ad3f5222326797803392b092fef36
Santiago Restrepo <srestrepoo@github.com>
2019-10-27 13:20:04 -0500

    Add search by name

--- src/main/java/com/training/petfood/test/UserController.java
+++ src/main/java/com/training/petfood/test/UserController.java
@@ -8,37 +8,43 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/usertest")
+    @GetMapping()
     public List<UserDto> getAll(){
         return userRepository.findAll();
     }
 
-    @GetMapping("/usertest/{id}")
+    @GetMapping("/{id}")
     public Optional<UserDto> getById(@PathVariable String id){
         int userId = Integer.parseInt(id);
         return userRepository.findById(userId);
     }
 
-    @PostMapping("/usertest")
+    @GetMapping("/search")
+    public List<UserDto> getByName(@RequestParam String name){
+        return userRepository.findByName(name);
+    }
+
+    @PostMapping()
     public UserDto create(@RequestBody Map<String, String> body){
         String name = body.get("name");
         Integer salary = Integer.parseInt(body.get("salary"));
         return userRepository.save(new UserDto(name, salary));
     }
 
-    @DeleteMapping("/usertest/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public void delete(@PathVariable String id){
         int userId = Integer.parseInt(id);
         UserDto userToDelete = userRepository.findById(userId).orElse(null);
         userRepository.delete(userToDelete);
     }
 
-    @PutMapping("/usertest/{id}")
+    @PutMapping("/{id}")
     public UserDto update(@PathVariable String id, @RequestBody Map<String, String> body){
         int userId = Integer.parseInt(id);
         // getting blog
--- src/main/java/com/training/petfood/test/UserRepository.java
+++ src/main/java/com/training/petfood/test/UserRepository.java
@@ -2,8 +2,11 @@ package com.training.petfood.test;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDto, Integer> {
+
+   List<UserDto> findByName(String name);
 
 }
