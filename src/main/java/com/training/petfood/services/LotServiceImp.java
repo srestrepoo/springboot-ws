a5446b3ba1f1ec1b718347f1e3e700f896957471
Santiago Restrepo <srestrepoo@github.com>
2019-12-10 15:48:10 -0500

    Add flyway first version file

--- src/main/java/com/training/petfood/services/LotServiceImp.java
+++ src/main/java/com/training/petfood/services/LotServiceImp.java
@@ -56,6 +56,7 @@ public class LotServiceImp implements ILotService {
 
     @Override
     public List<Lot> getAllLots() {
-        return lotRepository.findAll();
+        List<Lot> lotList = lotRepository.findAll();
+        return lotList;
     }
 }
